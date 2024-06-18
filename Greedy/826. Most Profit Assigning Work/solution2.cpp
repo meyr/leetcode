1e8ccc972a177749c50fb5683738095841da3620
meyr <fyonyung@gmail.com>
2024-06-18 08:50:38 +0800

    Add  826. Most Profit Assigning Work --> more solutions

--- Greedy/826. Most Profit Assigning Work/solution2.cpp
+++ Greedy/826. Most Profit Assigning Work/solution2.cpp
@@ -25,3 +25,24 @@ public:
         return ans;
     }
 };
+
+// 2024/06/18
+// 和上面的概念相同
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        vector<vector<int>> dp; // dicciculty, profit
+        for(int i = 0; i < difficulty.size(); ++i) dp.push_back({difficulty[i], profit[i]});
+        sort(begin(dp), end(dp));
+        sort(begin(worker), end(worker));
+        int ans{}, maxprofit{};
+        for(int i = 0, j = 0; i < worker.size(); ++i) { // O(M + N) 因為worker和dp都會只走過一次
+            while(j < dp.size() && worker[i] >= dp[j][0])
+                maxprofit = max(maxprofit, dp[j++][1]);
+            ans += maxprofit;
+        }
+        return ans;
+    }
+    // time : O(N + NlogN + MlogM + M + N) = O(NlogN + MlogM)
+    // space : O(N + logN + logM) = O(N)
+};
