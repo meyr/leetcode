660107d61b64d6abb5762a73a67f11e54357eead
fyon <fyonyung@gmail.com>
2024-11-10 22:16:33 +0800

    Add 3097. another solution using bitset

--- SlidingWindow/3097. Shortest Subarray With OR at Least K II/solution.cpp
+++ SlidingWindow/3097. Shortest Subarray With OR at Least K II/solution.cpp
@@ -36,3 +36,32 @@ public:
         return ans == sz + 1 ? -1 : ans;
     }
 };
+/*  2024/11/10 daily challenge
+ *  想法和上面一樣使用slinding window和bit manipulation來解題
+ *  但是改用bitset來做可以比較精簡而且比較不會有bug
+ */
+ class Solution {
+    vector<int> cnt(32);
+    void check(bitset<32>& xorval, bitset<32> val, int op) {
+        for(int i = 0; i < 32; ++i) {
+            if(val.test(i)) {
+                cnt[i] += op;
+                xorval.set(i, cnt[i] > 0);
+            }
+        }
+    }
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        if(k == 0) return 1; // special case
+        bitset<32> xorval;
+        int sz = nums.size(), ans = sz + 1;
+        for(int left = 0, right = 0; right < sz; ++right) {
+            check(xorval, nums[right], 1);
+            while(left <= right && xorval.to_ulong() >= k) {
+                ans = min(ans, right - left + 1);
+                check(xorval, nums[left++], -1);
+            }
+        }
+        return ans > sz ? -1 : ans;
+    }
+};
