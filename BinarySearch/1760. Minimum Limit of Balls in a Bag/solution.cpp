f535ab3ff05cdc67c0c022e83dc2095e47b10cf6
meyr <fyonyung@gmail.com>
2025-02-12 09:37:40 +0800

    Add 1760. daily challeng -- add more comments

--- BinarySearch/1760. Minimum Limit of Balls in a Bag/solution.cpp
+++ BinarySearch/1760. Minimum Limit of Balls in a Bag/solution.cpp
@@ -23,3 +23,38 @@ public:
         return left;
     }
 };
+/* 2025/02/12 daily challenge
+ *  maximum minimize problem --> binary search
+ *
+ *   penalty : 1, 2, 3, 4, 5, 6, 7
+ *   ops     : 8, 7, 6, 6, [5], 5, 4
+ *                      >   <=
+ *
+ *   9 -> 1, 8 -> 1, 1, 7 -> 1, 1, 1, 6 -> 1, 1, 1, 1, 5 -> 1, 1, 1, 1, 1, 4 -> 1, 1, 1, 1, 1, 1, 3 -> 1, 1, 1, 1, 1, 1, 1, 2 -> 1, 1, 1, 1, 1, 1, 1, 1, 1
+ *   9 -> 2, 7 -> 2, 2, 5 -> 2, 2, 2, 3 -> 2, 2, 2, 2, 1
+ *   9 -> 3, 6 -> 3, 3, 3
+ *   9 -> 4, 5 -> 4, 4, 1
+ *   9 -> 5, 4
+ *   9 -> 6, 3
+ *   9 -> 7, 2
+ *   9 -> 8, 1
+ *   9 -> 
+ *
+ *   time  : O(N + NlogM) = O(NlogM), N : sizeof nums, M : maximum num in nums
+ *   space : O(1)
+ */
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int left = 1, right = *max_element(begin(nums), end(nums));
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            int cnt{};
+            for(auto&　n : nums)
+                    cnt += n / mid - !(n % mid);
+            if(cnt <= maxOperations) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
+};
