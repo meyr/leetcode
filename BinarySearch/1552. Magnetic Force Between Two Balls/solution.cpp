1e6b6fea51d854f8a3d719cd6c27e519abd51e2e
meyr <fyonyung@gmail.com>
2025-02-14 10:10:14 +0800

    Add 1552. daily challenge

--- BinarySearch/1552. Magnetic Force Between Two Balls/solution.cpp
+++ BinarySearch/1552. Magnetic Force Between Two Balls/solution.cpp
@@ -34,3 +34,33 @@ public:
         return left - 1;
     }
 };
+/* 2025/02/14 daily challenge
+ *
+ *  minimum mangetic force between any two balls is maximum -> maximum-minimum problem -> binary search
+ *  
+ *  minimum magnetic force between any two balls : 1, 2, 3,  4, 5, 6
+ *                                    need balls : 5, 5, 4, [4], 3, 2, m = 4 --> take m == 4 and force is maximum
+ *                                                           >=  <
+ *  time  : O(NlogN + NlogM), N : size of position, M : max position - min position
+ *  space : O(logN)
+ */
+class Solution {
+public:
+    int maxDistance(vector<int>& position, int m) {
+        sort(begin(position), end(position));
+        int left = 1, right = position.back() - position.front() + 1;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            int cnt{1}, prev = position[0];
+            for(int i = 1; i < position.size(); ++i)
+                if(position[i] - prev >= mid) {
+                    cnt++;
+                    prev = position[i];
+                }
+            if(cnt < m) right = mid;
+            else left = mid + 1;
+
+        }
+        return left - 1;
+    }
+};
