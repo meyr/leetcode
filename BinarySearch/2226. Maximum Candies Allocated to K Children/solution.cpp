53f01f68c0e5c918e26641f7b11a72c9d8825af5
meyr <fyonyung@gmail.com>
2025-03-14 08:52:06 +0800

    Add 2226. daily challenge

--- BinarySearch/2226. Maximum Candies Allocated to K Children/solution.cpp
+++ BinarySearch/2226. Maximum Candies Allocated to K Children/solution.cpp
@@ -25,3 +25,32 @@ public:
 	// space : O(1) 
     }
 };
+/*  2025/03/14 daily challenge
+ *  想法和以前類似 看到題目就想到是binary search
+ *
+ */
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        long long sum = accumulate(begin(candies), end(candies), 0L);
+        if(k > sum) return 0;
+        else if(k == sum) return 1;
+        else {
+            int left = 1, right = *max_element(begin(candies), end(candies)) + 1;   // 參考以前的解答 right = sum / k + 1會比較好
+            while(left < right) {
+                int mid = left + (right - left) / 2;
+                long long cnt{};
+                for(auto& c : candies)
+                    cnt += c / mid;
+                if(cnt < k) right = mid;
+                else left = mid + 1;
+            }
+            return left - 1;
+        }
+    }
+};
+/*
+    mid : 1, 2, 3,  4,  5, 6
+    cnt : 8, 7, 6, [6], 5, 5
+                    >=  <
+*/
