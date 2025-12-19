778c0928f9db49edc58c07283d1f93dada0a8b14
meyr <fyonyung@gmail.com>
2025-12-19 10:43:51 +0800

    Add 2092. more explaintation

--- UnionFind/2092. Find All People With Secret/solution.cpp
+++ UnionFind/2092. Find All People With Secret/solution.cpp
@@ -1,3 +1,60 @@
+/*  2025/12/19 daily challenge
+ *  再寫一次還是沒寫出來 這邊的重點是有了reset機制 參考了原本的解答
+ */
+class UnionFind{
+    vector<int> root, rank;
+    int find(int x) {
+        if(root[x] == x) return x;
+        else return root[x] = find(root[x]);
+    }
+public:
+    UnionFind(int sz): root(sz), rank(sz, 1) {
+        iota(begin(root), end(root), 0);
+    }
+    int un(int x, int y) {
+        x = find(x), y = find(y);
+        if(x == y) return 0;
+        if(rank[y] > rank[x]) swap(x, y);
+        root[y] = x;
+        return rank[x] += rank[y];
+    }
+    void reset(int x) { // 這邊的reset 只單純修改root是OK 但是rank就有點問題 如果是從一個group撥離 且撥離後group size > 1就必須要修正
+        root[x] = x;    // 也就是我們只處理整個group都解散的情況
+        rank[x] = 1;    // 在這一題的情況是 因為不屬於group 0(就是不知道秘密) 都會被解散 所以這樣做可以成立
+    }
+    bool connect(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
+class Solution {
+public:
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+        UnionFind uf(n);
+        uf.un(0, firstPerson);
+        sort(begin(meetings), end(meetings), [](auto& a, auto& b){
+            return a[2] < b[2];
+        });
+        for(int i = 0; i < meetings.size();) {
+            vector<int> ppl;
+            int time = meetings[i][2];
+            for(; i < meetings.size() && meetings[i][2] == time; ++i) {
+                uf.un(meetings[i][0], meetings[i][1]);
+                ppl.push_back(meetings[i][0]);
+                ppl.push_back(meetings[i][1]);
+            }
+            for(int n : ppl)
+                if(!uf.connect(n, 0))
+                    uf.reset(n);
+        }
+        vector<int> ans;
+        for(int i = 0; i < n; ++i)
+            if(uf.connect(i, 0))
+                ans.push_back(i);
+        return ans;
+    }
+};
+
 /*     
  *     參考答案: https://leetcode.com/problems/find-all-people-with-secret/solutions/1599815/c-union-find/
  *    
