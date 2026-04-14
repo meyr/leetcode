3e34b61a798f4d823988f8055a1f6be0f0ff0080
wenhungyang <wenhung.yang@linfiny.com>
2026-04-14 10:10:14 +0800

    Add 2463. daily challenge and more comments

--- DynamicProgramming/2463. Minimum Total Distance Traveled/solution.cpp
+++ DynamicProgramming/2463. Minimum Total Distance Traveled/solution.cpp
@@ -31,3 +31,79 @@ public:
         return helper(0, 0, 0, robot, factory);
     }
 };
+/*  2026/04/14 daily challenge
+ *  再寫一次還是沒想到解答
+ *  參考上面的答案
+ *
+ *  + 每個工廠固定修理某個區間的robot
+ *
+ */
+class Solution {
+    // 第r個robot, 第f個facotry
+    // k : 目前的factory修理了k個robot
+    enum status{
+        position,
+        limit
+    };
+    using ll = long long;
+    ll helper(const vector<int>& robot, const vector<vector<int>>& factory, int r, int f, int k) {
+        if(r == robot.size()) return 0; // 全部修完了
+        if(f == factory.size()) return 1e13; // 沒修完工廠就用完了
+        else if(~mem[r][f][k]) return mem[r][f][k];
+        ll goToNextFacotry = helper(robot, factory, r, f + 1, 0);  // 跳下一個工廠
+        ll fixThisRobot = k < factory[f][limit] ?                  // 這個工廠是不是還可以修理 如果是就修理此機器人
+            abs(robot[r] - factory[f][position]) + helper(robot, factory, r + 1, f, k + 1) : 1e13;
+        return mem[r][f][k] = min(goToNextFacotry, fixThisRobot);
+    }
+    vector<vector<vector<ll>>> mem;
+public:
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        sort(begin(robot), end(robot));
+        sort(begin(factory), end(factory));
+        int n = robot.size(), m = factory.size();
+        mem.resize(n, vector<vector<ll>>(m, vector<ll>(n, -1)));
+        return helper(robot, factory, 0, 0, 0);
+    }
+};
+/*  同上 不需要使用k 一個工廠的可能是 不修理, 修理1台,...修理k台
+ *  但是time complexity一樣是O(NMN)
+ */
+class Solution {
+    enum {
+        position,
+        limit
+    };
+    using ll = long long;
+    ll helper(const vector<int>& robot, const vector<vector<int>>& factory, int r, int f) {
+        if(r == robot.size()) return 0;
+        if(f == factory.size()) return 1e13;
+        else if(~mem[r][f]) return mem[r][f];
+        ll rtn = helper(robot, factory, r, f + 1);
+        ll sum{};
+        for(int k = 1; k <= min(factory[f][limit], (int)robot.size() - r); ++k) {
+            sum += abs(robot[r + k - 1] - factory[f][position]);
+            rtn = min(rtn, sum + helper(robot, factory, r + k, f + 1));
+        }
+        return mem[r][f] = rtn;
+    }
+    vector<vector<ll>> mem;
+public:
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        sort(begin(robot), end(robot));
+        sort(begin(factory), end(factory));
+        int n = robot.size(), m = factory.size();
+        mem.resize(n, vector<ll>(m, -1));
+        return helper(robot, factory, 0, 0);
+    }
+};
+/*  這題讓人思考何謂正確答案?
+ *
+ *  正確答案就是你嘗試過所有的可能之後 在這些可能的答案裡面挑選一個 符合你條件的答案
+ *  因為所有可能的組合你都嘗試過了 所以一定是正確答案
+ *
+ *  那什麼是所有可能的組合?
+ *
+ *  對這題來說就是 每個工廠都有可能對每個機器人修理  <-- 如果這樣列舉可能的答案會大的可怕
+ *                 因為是要求最小值 所以每個工廠會對連續機器人修理 <-- [0, k] 個連續的機器人修理
+ *
+ */
